5a52f0a0662bc3830b9fa88bbb4f5165d39cd9d4
Vyacheslav Voronenko <git@voronenko.info>
2016-03-25 18:31:12 +0200

    lampbox related recipes: doxygen, phpcs, phploc, phpmd

---
+++ sudoer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ 
+if [ "$(id -u)" == "0" ]; then
+echo "Installation must NOT be done under sudo"
+echo "use your regular user account"
+exit 1
+fi
+
+sudo apt-get -y install git curl
+
+SUDOERUSER="$(whoami)"
+SUDOERFILE="/etc/sudoers.d/$SUDOERUSER"
+
+sudo bash -c "touch $SUDOERFILE"
+sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: ALL > $SUDOERFILE"
+
+echo "===================================================================="
+echo "current user to SUDOERS w/o password"
+echo "don't  forget to remove settings after initial box configuration"
+echo "by removing file $SUDOERFILE"
+
+
