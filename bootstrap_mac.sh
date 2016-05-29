194782a7e7d8ab9c1dd608871abf91a397fa56e9
Vyacheslav Voronenko <git@voronenko.info>
2016-05-29 19:56:47 +0300

    chore(mac) also uses ansible 2.0 now

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -4,5 +4,5 @@
 brew install git
 brew install python
 pip install -U pip
-sudo -H pip install ansible==1.9.4
-sudo -H pip install prudentia==1.0
+sudo -H pip install ansible
+sudo -H pip install prudentia
