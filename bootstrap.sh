874f703c6d9cb3030e757dbf470f67ddee859f5d
Vyacheslav Voronenko <git@voronenko.info>
2016-06-06 21:17:50 +0300

    fix(16.04) bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 if [ "$(id -u)" == "0" ]; then
 echo "Installation must NOT be done under sudo"
 echo "use your regular user account"
@@ -14,7 +14,7 @@ SUDOERFILE="/etc/sudoers.d/$SUDOERUSER"
 sudo bash -c "touch $SUDOERFILE"
 sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: ALL > $SUDOERFILE"
 
-sudo apt-get -y install -y software-properties-common git python-dev wget
+sudo apt-get -y install -y software-properties-common git python-dev wget apt-transport-https libffi-dev libssl-dev
 sudo apt-get install -y python-pip
 sudo pip install -U pip
 sudo pip install ansible
@@ -23,7 +23,3 @@ echo "===================================================================="
 echo "current user to SUDOERS w/o password"
 echo "don't  forget to remove settings after initial box configuration"
 echo "by removing file $SUDOERFILE"
-  
-
-
-
