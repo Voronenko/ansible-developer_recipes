6572308845f3cebf28de77388115f5bbee00d2f2
Vyacheslav Voronenko <git@voronenko.info>
2017-09-06 21:25:47 +0200

    related to docker-gc - ability to pass environment

--- sudoer.sh
+++ sudoer.sh
@@ -17,7 +17,7 @@ sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: /usr/bin/truecrypt >> $SUDO
 sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: /bin/systemctl >> $SUDOERFILE"
 sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown >> $SUDOERFILE"
 sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: /etc/init.d/nginx, /etc/init.d/mysql, /etc/init.d/mongod, /etc/init.d/redis, /etc/init.d/php-fpm >> $SUDOERFILE"
-sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD: /usr/bin/docker, /usr/sbin/docker-gc >> $SUDOERFILE"
+sudo bash -c "echo $SUDOERUSER ALL=\(ALL\) NOPASSWD:SETENV: /usr/bin/docker, /usr/sbin/docker-gc >> $SUDOERFILE"
 
 echo "===================================================================="
 echo "current user to SUDOERS w/o password"
