bee4018c28910d284a5e2ecd96b645ae7ab572f8
Vyacheslav Voronenko <git@voronenko.info>
2017-12-15 09:14:07 +0100

    improve(ideS) upgraded ides versions

--- local.sh
+++ local.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ansible-playbook -i "localhost," -c local local.yml
+stty_orig=`stty -g`
+stty -echo
+read -p "become pass: " BECOME_PASS
+stty $stty_orig
+ansible-playbook -i "localhost," -c local local.yml --extra-vars "'ansible_become_pass=${BECOME_PASS}'"
